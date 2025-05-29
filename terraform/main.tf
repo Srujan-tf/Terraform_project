b989d5ba1894d1bf4427d66544d5a5ad2e8cd169
srujan-tf <srujanreddy4810@gmail.com>
2025-05-29 13:44:49 +1000

    added code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,51 @@
 provider "aws" {
   region = var.region
 }
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a" # adjust based on your region
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = file(var.public_key_path)
@@ -38,7 +83,7 @@ resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name = aws_key_pair.deployer.key_name
-  subnet_id = subnet-05fe6a7034ade54bb
+  subnet_id = aws_subnet.public_subnet.id
   vpc_security_group_ids = [ aws_security_group.app_sg.id ]
   user_data = <<-EOF
               #!/bin/bash
--- terraform/output.tf
+++ terraform/output.tf
@@ -2,3 +2,16 @@ output "instance_public_ip" {
   description = "The public IP address of the EC2 instance"
   value       = aws_instance.web.public_ip
 }
+output "instance_id" {
+  description = "The id of instance"
+  value = aws_instance.web.id
+}
+output "vpc_id" {
+  description = "The id of vpc"
+  value = aws_vpc.main_vpc.id
+  
+}
+output "subnet_id" {
+ description = "The id of subnet"
+ value = aws_subnet.public_subnet.id
+}
